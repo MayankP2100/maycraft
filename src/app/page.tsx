c559aab6714ff2dba9d1d78665e67247121cce3d
Mayank Pal <palmayank2100@gmail.com>
2025-07-10 14:32:34 +0530

    feat: implement TRPC with React Query and add AI greeting functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div>Hello World!</div>;
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "world" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
